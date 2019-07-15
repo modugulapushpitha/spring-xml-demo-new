c349dbadfbf42248d374f0969149c9bab8215c3d
modugulapushpitha <pushpithamodugula@gmail.com>
2019-07-15 21:07:12 +0530

    Task 3 added

--- src/main/java/com/stackroute/spring/Main.java
+++ src/main/java/com/stackroute/spring/Main.java
@@ -4,25 +4,14 @@ import com.stackroute.spring.domain.Movie;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
         ApplicationContext obj = new ClassPathXmlApplicationContext("bean.xml");
-       // BeanFactory obj2=new ClassPathXmlApplicationContext("bean.xml");
-
-        Movie ac1= obj.getBean("mobj1", Movie.class);
-        //ac1.displaydemo();
-        Movie ac2= obj.getBean("mobj1", Movie.class);
-        //ac2.displaydemo();
-        Movie ac3= obj.getBean("mobj2", Movie.class);
-       // ac3.displaydemo();
-        if(ac1==ac2)
-            System.out.println(true);
-        if(ac2==ac3)
-            System.out.println(true);
-        if(ac3==ac1)
-            System.out.println(true);
-
-
+        Movie m1=obj.getBean("mobj1",Movie.class);
+        m1.displaydemo();
+        Movie m2=obj.getBean("mobj2",Movie.class);
+        m2.displaydemo();
 
     }
 }
--- src/main/java/com/stackroute/spring/domain/Movie.java
+++ src/main/java/com/stackroute/spring/domain/Movie.java
@@ -3,17 +3,17 @@ package com.stackroute.spring.domain;
 public class Movie {
     private String mname;
     private Actor aobj;
-    public Movie(String mname,Actor aobj){
+    /* public Movie(String mname,Actor aobj){
         this.mname=mname;
         this.aobj=aobj;
-    }
-   /*public void setMname(String mname) {
+    } */
+   public void setMname(String mname) {
         this.mname = mname;
     }
 
     public void setAobj(Actor aobj){
         this.aobj=aobj;
-    }   */
+    }
     public void displaydemo(){
         System.out.println("Details of Movie:" + mname);
         aobj.dispaly();
